c70c90e651394529bb61ae9c3869c99b5b99b8f9
gittemcode <u1355687@umail.utah.edu>
2025-02-03 23:59:59 -0700

    Website is up and running

---
+++ tai-frontend/src/pages/HomePage.js
@@ -0,0 +1,53 @@
+// TODO: Split into a chat component and a file upload component for SOC
+
+import React, { useState } from 'react';
+
+const HomePage = () => {
+  const [file, setFile] = useState(null);
+  const [message, setMessage] = useState('');
+
+  const handleFileChange = (event) => {
+    const uploadedFile = event.target.files[0];
+    setFile(uploadedFile);
+  };
+
+  const handleUpload = () => {
+    if (file) {
+      console.log("File selected:", file.name);
+      // Can upload to GPT here for files 
+    } else {
+      console.log("No file selected");
+    }
+  };
+
+  // This needs to be here to update the message that is being typed
+  const handleMessageChange = (event) => {
+    setMessage(event.target.value);
+  };
+
+  const handleMessage = () => {
+    // Can upload messages here 
+    console.log("Message:", message);
+    setMessage("");
+  };
+
+  return (
+    <div>
+      <h1>This is the home page</h1>
+      <div>
+        <input type="file" onChange={handleFileChange} />
+        <button onClick={handleUpload}>Upload File</button>
+      </div>
+      <div>
+        <textarea
+          value={message}
+          onChange={handleMessageChange}
+        />
+        <button onClick={handleMessage}>Send Message</button>
+      </div>
+      {file && <p>Selected file: {file.name}</p>}
+    </div>
+  );
+};
+
+export default HomePage;
