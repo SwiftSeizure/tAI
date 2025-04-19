37667561182c58695941390651346fdc81af3cf1
gittemcode <u1355687@umail.utah.edu>
2025-04-19 13:29:02 -0600

    modified module component to begin passing down information.

--- tai-frontend/src/components/ModuleComponent.js
+++ tai-frontend/src/components/ModuleComponent.js
@@ -1,6 +1,16 @@
 import React from "react"; 
 
-const ModuleComponent = () => { 
+// Devide what we need to pass down to the module here maybe the entire module 
+// then do all the conditionals to map it in here if the things exist 
+const ModuleComponent = ( { } ) => {  
+
+    return ( 
+        <>
+        <div> 
+            This is a module div 
+        </div>
+        </>
+    )
 
 }; 
 
