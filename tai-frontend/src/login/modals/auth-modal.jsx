edca2f6e17a196dbc32399145b8fed4924335fd3
gittemcode <u1355687@umail.utah.edu>
2025-09-08 13:45:29 -0600

    Installed firebase in npm packages

---
+++ tai-frontend/src/login/modals/auth-modal.jsx
@@ -0,0 +1,11 @@
+import { useNavigate } from 'react-router-dom';    
+
+export const AuthModal = () => { 
+    const navigate = useNavigate();
+    
+    return (
+        <div>
+            <h1>Auth Modal</h1>
+        </div>
+    )
+}
