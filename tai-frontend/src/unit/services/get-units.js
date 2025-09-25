2b113792fb7384480e3f90d8a7b9b997ac5b572f
gittemcode <u1355687@umail.utah.edu>
2025-09-25 11:48:59 -0600

    Missed route

--- tai-frontend/src/unit/services/get-units.js
+++ tai-frontend/src/unit/services/get-units.js
@@ -1,8 +1,6 @@
-import axios from 'axios';
-import { API_BASE_URL } from "../../shared/constants/urls";
+import api from "../../shared/services/axios";
 
 export const getUnits = async (classId) => {
-  const response = await axios.get(`${API_BASE_URL}/classroom/${classId}/units`);
+  const response = await api.get(`/classroom/${classId}/units`);
   return response.data.units;
 };
-
