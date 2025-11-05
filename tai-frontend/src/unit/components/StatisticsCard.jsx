00a018651541e87b7adc266cfcb7d167d25cbfa9
gittemcode <u1355687@umail.utah.edu>
2025-11-05 12:21:28 -0700

    Added some basics for local development (will need to be changed on main)

--- tai-frontend/src/unit/components/StatisticsCard.jsx
+++ tai-frontend/src/unit/components/StatisticsCard.jsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import React from 'react'; 
+
 
 export const StatisticsCard = () => {
     return (
         <div>
-            <h1>Statistics Card</h1>
+            <h1>Statistics Card</h1> 
+
         </div>
     );
 }
