5628158c45d4fb1d9d5f449cf99a0a9a9bcffc0f
gittemcode <u1355687@umail.utah.edu>
2025-11-17 22:22:23 -0700

    attempting

--- tai-frontend/src/login/services/create-student.js
+++ tai-frontend/src/login/services/create-student.js
@@ -1,15 +1,12 @@
 import api from "../../shared/services/axios";
-import { auth } from "../../auth/firebase";
 
 export const createStudent = async (name, username) => { 
-    const idToken = await auth.currentUser?.getIdToken();
     try {
         const response = await api.post('/student/new', {
             name: name,
             username: username
         }, {
             headers: {
-                'Authorization': `Bearer ${idToken}`,
                 'Content-Type': 'application/json',
             },
         });
--- tai-frontend/src/login/services/create-teacher.js
+++ tai-frontend/src/login/services/create-teacher.js
@@ -1,19 +1,13 @@
 import api from "../../shared/services/axios";
-import { auth } from "../../auth/firebase";
 
 export const createTeacher = async (name, username) => {
     try {
-        const idToken = await auth.currentUser?.getIdToken();
-        if (!idToken) {
-            throw new Error('No authentication token found');
-        }
 
         const response = await api.post(`/teacher/new`, {
             name: name,
             username: username
         }, {
             headers: {
-                'Authorization': `Bearer ${idToken}`,
                 'Content-Type': 'application/json'
             }
         });
